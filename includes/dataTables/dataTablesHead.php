dee3f6a4eb206fa41de8b82d8dec44332ac27235
alexsandron <alexsandro060299@outlook.com>
2021-03-24 17:29:02 -0300

    Hotjar Teste

--- includes/dataTables/dataTablesHead.php
+++ includes/dataTables/dataTablesHead.php
@@ -87,3 +87,16 @@
 
   gtag('config', 'G-8T3KQC9WL8');
 </script>
+
+
+<!-- Hotjar Tracking Code for http://fabiopasseios.com.br/ -->
+    <script>
+    (function(h,o,t,j,a,r){
+        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+        h._hjSettings={hjid:2315258,hjsv:6};
+        a=o.getElementsByTagName('head')[0];
+        r=o.createElement('script');r.async=1;
+        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+        a.appendChild(r);
+    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+</script>
