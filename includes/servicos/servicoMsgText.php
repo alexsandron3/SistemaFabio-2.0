ed75a146672116654bc69b2ca18d4777d6854d08
alexsandron <alexsandro060299@outlook.com>
2021-05-06 20:04:12 -0300

    Correção de bug em que estava mostrando a data incorreta

--- includes/servicos/servicoMsgText.php
+++ includes/servicos/servicoMsgText.php
@@ -65,7 +65,7 @@ function mensagensInfoNoSession($texto){
         <button type='button' class='close' data-dismiss='alert' aria-label='Close'>
             <span aria-hidden='true'><i class='material-icons'>clear</i></span>
         </button>
-        <b></b> <h4> $texto</h4>
+        <b></b> <h4 class='text-secondary'> $texto</h4>
 
         </div>
     </div>";    
