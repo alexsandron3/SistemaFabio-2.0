de9843a6a65c8e09c1e2e267b941c04b3da86834
alexsandron <alexsandro060299@outlook.com>
2021-07-06 19:11:49 -0300

    Adicionada função de ocultar/exibir clientes desativados e adicionado campo TRANSPORTE no relatório de pontos de embarque

--- pontosDeEmbarque.php
+++ pontosDeEmbarque.php
@@ -6,7 +6,7 @@ include_once("./includes/header.php");
 $idPasseioGet = filter_input(INPUT_GET, 'id', FILTER_SANITIZE_NUMBER_INT);
 /* -----------------------------------------------------------------------------------------------------  */
 
-$queryBuscaPeloIdPasseio = "SELECT  p.nomePasseio, p.idPasseio, c.nomeCliente, c.idCliente, c.poltrona, pp.localEmbarque, pp.anotacoes, c.referencia
+$queryBuscaPeloIdPasseio = "SELECT  p.nomePasseio, p.idPasseio, c.nomeCliente, c.idCliente, c.poltrona, pp.localEmbarque, pp.anotacoes, c.referencia, pp.transporte
                               FROM passeio p, pagamento_passeio pp, cliente c WHERE pp.idPasseio='$idPasseioGet' AND pp.idPasseio=p.idPasseio AND pp.idCliente=c.idCliente AND pp.statusPagamento NOT IN(0) AND pp.clienteDesistente NOT IN(1) ORDER BY localEmbarque";
 $resultadoBuscaPasseio = mysqli_query($conexao, $queryBuscaPeloIdPasseio);
 /* -----------------------------------------------------------------------------------------------------  */
@@ -64,6 +64,7 @@ $dataPasseio = date_create($rowpegarNomePasseio['dataPasseio']);
                   <th>ANOTAÇÕES</th>
                   <th>REFERENCIA</th>
                   <th>POLTRONA</th>
+                  <th>TRANSPORTE</th>
                 </tr>
               </thead>
 
@@ -83,6 +84,7 @@ $dataPasseio = date_create($rowpegarNomePasseio['dataPasseio']);
                     <td><?php echo $rowBuscaPasseio['anotacoes'] . "<BR/>"; ?></td>
                     <td><?php echo $rowBuscaPasseio['referencia'] . "<BR/>"; ?></td>
                     <td><?php echo $rowBuscaPasseio['poltrona'] . "<BR/>"; ?></td>
+                    <td><?php echo $rowBuscaPasseio['transporte'] . "<BR/>"; ?></td>
                   </tr>
 
                 <?php
