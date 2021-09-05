ffd0874cccb02aca460a25aaae4d2d7686950ab0
Alexsandro Xavier <alexsandro060299@outlook.com>
2021-09-05 19:37:17 -0300

    Teste passou

--- includes/plugins/DataTables/configFiles/dataTablesRelVendas.js
+++ includes/plugins/DataTables/configFiles/dataTablesRelVendas.js
@@ -1,11 +1,38 @@
+
 $(document).ready(function() {
-  $.fn.dataTable.moment('DD/MM/YYYY'); //Formatação sem Hora
-  $('#relatorioVendasTable').DataTable({
+  // $.fn.dataTable.moment('DD/MM/YYYY'); //Formatação sem Hora
+  // const value = $('#inicio').val();
+  // console.log(value);
+
+  const table = $('#relatorioVendasTable').DataTable(
+    {
     "processing": true,
-    // "serverSide": true,
+    "serverSide": true,
+    // "searching": false,
+    "filter": true,
     "ajax": {
-      "type": "GET",
       "url": "./teste-backend-search.php",
+      "type": "POST",
+      "datatype": "json",
+      
+      data: {
+        "inicio": function (data) {
+          console.log(data);
+          // Lendo valores
+          const inicio = $('#inicio').val();
+          const today = new Date();
+          const inputDate = new Date(inicio);
+          today.setHours(0,0,0,0)
+          inputDate.setHours(0,0,0,0)
+          inputDate.setDate(inputDate.getDate() +1)
+          if (moment(inputDate).isAfter(today)){
+            return moment(today).format('YYYY-MM-DD');
+          }
+          return inicio;
+          
+        },
+        "fim": "2030-09-06"
+      },
       dataSrc: '',
     },
     "columns": [
@@ -14,6 +41,10 @@ $(document).ready(function() {
       {"data": "NVendas"},
       {"data": "ValorVenda"},
       {"data": "ValorPago"},
-  ]
+    ]
   });
+  
+  $('#inicio').change(function () {
+    table.draw();
+  })
 });
