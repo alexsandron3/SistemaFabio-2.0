43e13ef883c3d508fd35d10c66a83d3696eea1d9
Alexsandro Xavier <alexsandro060299@outlook.com>
2021-09-05 19:38:17 -0300

    Teste passou

--- includes/htmlElements/navbar.php
+++ includes/htmlElements/navbar.php
@@ -1,4 +1,4 @@
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+<!-- <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
     <div class="d-flex justify-content-lg-start ml-2">
         <a class="btn-just-icon " onclick="history.go(-1)">
             <i class="material-icons">
@@ -85,4 +85,148 @@
             </ul>
         </div>
     </div>
+</nav> -->
+
+
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark" style="background-color:#212121 !important;">
+    <!-- Container wrapper -->
+    <div class="container-fluid">
+        <div class="d-flex justify-content-lg-start ml-2 mr-3">
+            <a class="btn-just-icon " onclick="history.go(-1)">
+                <i class="material-icons">
+
+                    <span class="material-icons-outlined">
+                        arrow_back
+                    </span>
+                </i>
+            </a>
+            <a class="btn-just-icon  ml-3" onclick="history.go(+1)">
+                <i class="material-icons">
+
+                    <span class="material-icons-outlined">
+                        arrow_forward
+                    </span>
+                </i>
+            </a>
+            <a class="btn-just-icon ml-3" onclick="history.go(0)">
+                <i class="material-icons mx-auto my-auto">
+                    refresh
+                </i>
+            </a>
+        </div>
+        <!-- Navbar brand -->
+        <!-- <a class="navbar-brand" href="#">Início</a> -->
+
+        <!-- Toggle button -->
+        <button class="navbar-toggler" type="button" data-mdb-toggle="collapse" data-mdb-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <i class="fas fa-bars"></i>
+        </button>
+
+        <!-- Collapsible wrapper -->
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+
+                <!-- Link -->
+                <li class="nav-item">
+                    <a class="nav-link" href="index.php">INÍCIO</a>
+                </li>
+
+                <!-- CADASTRAR -->
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-mdb-toggle="dropdown" aria-expanded="false">
+                        CADASTRAR
+                    </a>
+                    <!-- Dropdown menu -->
+                    <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <li>
+                            <a class="dropdown-item" href="cadastroCliente.php">CLIENTE</a>
+                        </li>
+                        <li>
+                            <a class="dropdown-item" href="cadastroDespesas.php">DESPESAS</a>
+                        </li>
+                        <li>
+                            <a class="dropdown-item" href="cadastroPasseio.php">PASSEIO</a>
+                        </li>
+                        <!-- <li>
+                            <hr class="dropdown-divider" />
+                        </li>
+                        <li>
+                            <a class="dropdown-item" href="#">Something else here</a>
+                        </li> -->
+                    </ul>
+                </li>
+
+                <!-- Link -->
+                <li class="nav-item">
+                    <a class="nav-link" href="relatoriosPasseio.php">PASSEIOS</a>
+                </li>
+                <!-- PESQUISAR -->
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-mdb-toggle="dropdown" aria-expanded="false">
+                        PESQUISAR
+                    </a>
+                    <!-- Dropdown menu -->
+                    <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <li>
+                            <a class="dropdown-item" href="pesquisarCliente.php">CLIENTE</a>
+                        </li>
+                        <li>
+                            <a class="dropdown-item" href="pesquisarPasseio.php">PASSEIO</a>
+                        </li>
+                    </ul>
+                </li>
+
+                <!-- RELATÓRIOS -->
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-mdb-toggle="dropdown" aria-expanded="false">
+                        RELATÓRIOS
+                    </a>
+                    <!-- Dropdown menu -->
+                    <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <li>
+                            <a class="dropdown-item" href="listaAniversariantesMes.php">ANIVERSARIANTES DO MÊS</a>
+                        </li>
+                        <li>
+                            <a class="dropdown-item" href="log.php">LOGS</a>
+                        </li>
+                        <li>
+                            <a class="dropdown-item" href="listaPagamentosPendentes.php">PAGAMENTOS PENDENTES</a>
+                        </li>
+                        <li>
+                            <a class="dropdown-item" href="pesquisarPagamentos.php">PAGAMENTOS REALIZADOS</a>
+                        </li>
+                        <li>
+                            <a class="dropdown-item" href="relatorioPeriodico.php">RELATÓRIO PERIÓDICO DE VENDAS</a>
+                        </li>
+                        <li>
+                            <a class="dropdown-item" href="relatorioVendas.php">RELATÓRIO DE VENDAS</a>
+                        </li>
+                        <li>
+                            <hr class="dropdown-divider">
+                        </li>
+                        <li>
+                            <a class="dropdown-item" href="teste.php">RELATÓRIO VENDAS ADICIONAL</a>
+                        </li>
+                    </ul>
+                </li>
+            </ul>
+
+            <!-- Icons -->
+            <ul class="navbar-nav d-flex flex-row me-1">
+                <li class="nav-item me-3 me-lg-0">
+                    <a class="nav-link" href="#"><i class="fas fa-shopping-cart"></i></a>
+                </li>
+                <li class="nav-item me-3 me-lg-0">
+                    <a class="nav-link" href="#"><i class="fab fa-twitter"></i></a>
+                </li>
+            </ul>
+
+            <!-- Search -->
+            <!-- <form class="w-auto">
+                <input type="search" class="form-control" placeholder="Type query" aria-label="Search">
+            </form> -->
+
+        </div>
+    </div>
+    <!-- Container wrapper -->
 </nav>
---
+++ includes/mdbJs.php
@@ -0,0 +1,14 @@
+<!-- <script type="text/javascript" src="./includes/MDB/js/mdb.min.js"></script> -->
+
+<!-- datePicker -->
+<!-- <script src="https://code.jquery.com/jquery-1.8.2.js"></script> -->
+<!-- <script src="https://code.jquery.com/ui/1.9.0/jquery-ui.js"></script> -->
+
+<!-- <script src="teste2.js"></script> -->
+
+<!-- DATATABLES -->
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.36/pdfmake.min.js"></script>
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.36/vfs_fonts.js"></script>
+<script type="text/javascript" src="https://cdn.datatables.net/v/dt/jq-3.3.1/jszip-2.5.0/dt-1.11.1/af-2.3.7/b-2.0.0/b-colvis-2.0.0/b-html5-2.0.0/b-print-2.0.0/cr-1.5.4/date-1.1.1/fc-3.3.3/fh-3.1.9/kt-2.6.4/r-2.2.9/rg-1.1.3/rr-1.2.8/sc-2.0.5/sb-1.2.0/sp-1.4.0/sl-1.3.3/datatables.min.js"></script>
+<script src="includes/plugins/DataTables/configFiles/dataTablesRelVendas.js"> </script>
+<script type="text/javascript" src="https://momentjs.com/downloads/moment-with-locales.js"></script>
