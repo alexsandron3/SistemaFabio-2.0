9501347f44ce99065b8c31d1f893213e7dff64a8
Alexsandro Xavier <alexsandro060299@outlook.com>
2021-10-11 16:56:13 -0300

    Cria API para buscar pagamentos

--- api/pagamento.php
+++ api/pagamento.php
@@ -8,6 +8,8 @@
   header('Access-Control-Allow-Methods: GET, POST');
   header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token');
   $apiAnswer = array();
+  $time_start = microtime(true);
+
   // $showInactives = 0;
   // $inicio = null;
   // $fim = null;
@@ -26,6 +28,35 @@
   // }else {
   //   $apiAnswer =  selectAll($conn, $showInactives, $data);
   // }
-  $apiAnswer = countStatus($conn, $_GET['id']);
+  // $apiAnswer = countStatus($conn, $_GET['id']);
+  $passeioId = "SELECT idPasseio, nomePasseio, dataPasseio, lotacao FROM passeio";
+  $stmt = $conn->prepare($passeioId);
+  $response = executeSelect($stmt);
+  $apiAnswer = $response;
+
+  // while ($row = ) {
+  //   // $id = $row;
+  // }
+  $apiAnswer['passeio'] = $response['serverResponse']['sql']->fetch_all(MYSQLI_ASSOC);
+  // while ($row = $response['serverResponse']['sql']->fetch_array(MYSQLI_ASSOC)) {
+  //   // $id = $row;
+  //   $apiAnswer['passeio'][] = $row;
+  // }
+  foreach ($apiAnswer['passeio'] as $key => $value) {
+    // print_r($value['idPasseio']);
+    $id = $value['idPasseio'];
+    // print_r($row);
+    $query = "SELECT pp.statusPagamento, COUNT(*) AS pagamentos FROM pagamento_passeio pp WHERE pp.idPasseio = $id GROUP BY statusPagamento";
+    $stmt1 = $conn->prepare($query);
+    $response1 = executeSelect($stmt1);
+    while($result = $response1['serverResponse']['sql']->fetch_array(MYSQLI_ASSOC)) {
+      $apiAnswer['passeio'][$key]['pagamentos'][] = $result;
+
+    }
+  }
+
+$time_end = microtime(true);
+$execution_time = ($time_end - $time_start);
+$apiAnswer['serverResponse']['execTime'] = $execution_time;
 
 echo json_encode($apiAnswer);
