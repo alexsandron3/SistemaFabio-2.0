b284f9ee887c4a66f5277954a0a81197599d6f52
Alexsandro Xavier <alexsandro060299@outlook.com>
2021-10-18 15:54:03 -0300

    Adiciona update em API de passeio

--- api/passeio.php
+++ api/passeio.php
@@ -2,7 +2,7 @@
   include_once('../includes/header.php');
   require __DIR__.'/classes/Database.php';
   header('Access-Control-Allow-Headers: access');
-  header('Access-Control-Allow-Methods: GET, POST');
+  header('Access-Control-Allow-Methods: GET, POST, UPDATE');
   header('Content-Type: application/json; charset=UTF-8');
   header('Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With');
   header('Access-Control-Allow-Origin: *');
@@ -15,40 +15,44 @@
   $returnData = [];
 
   if($_SERVER['REQUEST_METHOD'] === 'GET') {
-    if(isset($_GET['pesquisarPasseio'])){
-      $wordToSearch = "%{$_GET['pesquisarPasseio']}%";
+    if(isset($_GET['id'])){
+      // if()
+      $fetch_passeio = "SELECT * FROM passeio WHERE idPasseio = :wordToSearch";
+      $stmt = $conn->prepare($fetch_passeio);
+      $wordToSearch = $_GET['id'];
     }else{
-      $wordToSearch = "% %";
-    }
-
-    try {
+      if(isset($_GET['pesquisarPasseio'])){
+        $wordToSearch = "%{$_GET['pesquisarPasseio']}%";
+      }else{
+        $wordToSearch = "% %";
+      }
       $fetch_passeio = "SELECT * FROM passeio WHERE nomePasseio LIKE :wordToSearch OR localPasseio LIKE :wordToSearch OR idPasseio LIKE :wordToSearch";
       $stmt = $conn->prepare($fetch_passeio);
-      $stmt->bindValue(':wordToSearch', $wordToSearch, PDO::PARAM_STR);
-      $stmt->execute();
-
-      if($stmt->rowCount()){
-        $row = $stmt->fetchAll(PDO::FETCH_ASSOC);
-        $returnData = [
-          "success" => 1,
-          "message" => 'Pesquisa realizada com sucesso!',
-          "passeio" => $row
-        ];
-      }else {
-        $returnData = msg(0, 422, 'Passeio não encontrado!');
-      }
-    } catch (\Throwable $e) {
-      $returnData = msg(0,500,$e->getMessage());
-
     }
+      try {
+        $stmt->bindValue(':wordToSearch', $wordToSearch, PDO::PARAM_STR);
+        $stmt->execute();
+  
+        if($stmt->rowCount()){
+          $row = $stmt->fetchAll(PDO::FETCH_ASSOC);
+          $returnData = [
+            "success" => 1,
+            "message" => 'Pesquisa realizada com sucesso!',
+            "passeio" => $row
+          ];
+        }else {
+          $returnData = msg(0, 422, 'Passeio não encontrado!');
+        }
+      } catch (\Throwable $e) {
+        $returnData = msg(0,500,$e->getMessage());
+  
+      }
+    
 
-  }else{
-    // echo gettype();
+  }elseif($_SERVER['REQUEST_METHOD'] === 'POST'){
     try {
       $verify_passeio = "SELECT nomePasseio, dataPasseio, idPasseio FROM passeio WHERE nomePasseio = :nomePasseio AND dataPasseio = :dataPasseio";
       $stmt = $conn->prepare($verify_passeio);
-      // $nomePasseio = $data->nomePasseio;
-      // $dataPasseio = $data->dataPasseio;
       $stmt->bindValue(':nomePasseio', $data->nomePasseio, PDO::PARAM_STR);
       $stmt->bindValue(':dataPasseio', $data->dataPasseio, PDO::PARAM_STR);
       $stmt->execute();
@@ -77,7 +81,23 @@
       $returnData = msg(0,500,$e->getMessage());
 
     }
-    // return json_encode($returnData);
+  }elseif ($_SERVER['REQUEST_METHOD'] === 'UPDATE') {
+    $update_passeio = "UPDATE passeio SET anotacoes=:anotacoes, dataLancamento=:dataLancamento, dataPasseio=:dataPasseio, idadeIsencao=:idadeIsencao , itensPacote=:itensPacote ,localPasseio=:localPasseio ,lotacao=:lotacao ,nomePasseio=:nomePasseio ,prazoVigencia=:prazoVigencia ,statusPasseio=:statusPasseio ,valorPasseio=:valorPasseio WHERE idpasseio=:idPasseio";
+  // return print_r(json_encode($data));
+
+    $stmt = $conn->prepare($update_passeio);
+    $stmt->execute((array) $data);
+    if($stmt->rowCount()){
+      $returnData = [
+        "success" => 1,
+        "message" => 'Atualização realizada com sucesso!',
+      ];
+    }else{
+      $returnData = [
+        "success" => 0,
+        "message" => 'Alterações não realizadas, tente novamente ou entre em contato com o suporte!',
+      ];
+    }
 
   }
 
