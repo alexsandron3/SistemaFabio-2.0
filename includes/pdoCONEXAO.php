0db11ae3b4fdedfc7417103900a89e7e87fbe06f
Alexsandro Xavier da Silveira <62912497+alexsandron3@users.noreply.github.com>
2021-11-04 16:53:14 -0300

    Teste

--- includes/pdoCONEXAO.php
+++
@@ -1,7 +0,0 @@
-<?php $conn = new mysqli("localhost", "root", "", "fabiopasseios");
-        if($conn->connect_errno){
-            echo"FALHA AO SE CONECTAR COM O MYSQL: " . $conn->connect_error;
-            exit();
-        }
-
-?>
