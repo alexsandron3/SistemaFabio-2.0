591c27617dbc902d519421c45ce9b11d66d6a783
Alexsandro Xavier da Silveira <62912497+alexsandron3@users.noreply.github.com>
2021-11-04 17:07:21 -0300

    teste

---
+++ api/includes/header.php
@@ -0,0 +1,26 @@
+<?php
+    //CHARSET
+    header("Content-type: text/html; charset=utf-8");
+
+    //VERIFICAÇÃO DE SESSÃO
+    if(!isset($_SESSION)) 
+    { 
+        session_start(); 
+    } 
+    // VERIFICANDO SE USUÁRIO ESTÁ LOGADO
+    // if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
+    //     header("location: login.php");
+    //     exit;
+    // }
+
+
+    //ARQUIVOS NECESSÁRIOS PARA UM INCLUDE
+    include_once("conexao.php");
+    include_once("pdoCONEXAO.php");
+    include_once("functions.php");
+    include_once("servicos/servicoValidacaoPermissao.php");
+    include_once("servicos/servicoRedirecionamento.php");
+    include_once("servicos/servicoMsgText.php");
+    include_once("servicos/servicoMensagens.php");
+    include_once("constantes.php");
+    include_once("htmlElements/esconderTabelas.php");
