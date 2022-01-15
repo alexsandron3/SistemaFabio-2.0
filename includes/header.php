22e974b159be51568b2d596283cddfc38cba9746
Alexsandro Xavier da Silveira <62912497+alexsandron3@users.noreply.github.com>
2022-01-15 13:24:23 -0300

    Config vars setted, cleaning unused files

--- includes/header.php
+++ includes/header.php
@@ -1,7 +1,4 @@
 <?php
-
-    use Dotenv\Dotenv;
-    // echo __DIR__ . ;
     require  __DIR__ . '../../vendor/autoload.php';
     $dotenv = \Dotenv\Dotenv::createImmutable(__DIR__. '\..' );
     $dotenv->safeLoad();
@@ -13,20 +10,6 @@
     { 
         session_start(); 
     } 
-    // VERIFICANDO SE USUÁRIO ESTÁ LOGADO
-    // if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
-    //     header("location: login.php");
-    //     exit;
-    // }
-
-
-    //ARQUIVOS NECESSÁRIOS PARA UM INCLUDE
-    // include_once("conexao.php");
-    // include_once("pdoCONEXAO.php");
     include_once("functions.php");
-    // include_once("servicos/servicoValidacaoPermissao.php");
-    // include_once("servicos/servicoRedirecionamento.php");
-    // include_once("servicos/servicoMsgText.php");
-    // include_once("servicos/servicoMensagens.php");
+
     include_once("constantes.php");
-    // include_once("htmlElements/esconderTabelas.php");
